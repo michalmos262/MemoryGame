67a6eee8a7bcfff42b9ea959cbf88ea62847b3cf
Michal Moscovich <michal.mos262@gmail.com>
2024-06-01 23:16:15 +0300

    created card struct and fixed board

---
+++ MemoryGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using MemoryGame;
+using System;
+
+namespace Ex_02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GameBoard board = new GameBoard(6, 6);
+
+            for (int i = 0; i < board.NumOfRows; i++)
+            {
+                for (int j = 0; j < board.NumOfColumns; j++)
+                {
+                    Console.Write(board.Board[i, j].Number + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
